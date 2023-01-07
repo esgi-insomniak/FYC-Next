d6351e767061e6f4641bb07d42d38ec43620d05c
ThePrimesBros <thesket5@outlook.fr>
2023-01-07 14:24:19 +0100

    Correction du premier TP sur les fonctionnalitées de base de Next

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,70 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import styles from '../styles/layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-export default function Layout({children}:any) {
-    return <div className={styles.container}>{children}</div>;
+const name = 'Raphaël Bessonnier';
+export const siteTitle = 'Notre site';
+
+export default function Layout({ children, home }: { children: React.ReactNode; home?: boolean }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{siteTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Ceci est un test de site"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle,
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt=""
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <Image
+                priority
+                src="/profile.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt=""
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
 }
--- pages/posts/second-post.tsx
+++ pages/posts/second-post.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import Layout from '../../components/layout';
 
 export default function FirstPost() {
@@ -7,9 +6,9 @@ export default function FirstPost() {
         <>
             <Layout>
                 <Head>
-                    <title>First Post</title>
+                    <title>Second Post</title>
                 </Head>
-                <h1>First Post</h1>
+                <h1>Second Post</h1>
             </Layout>
         </>
     );
